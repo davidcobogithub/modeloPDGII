15533a6d47ba6b20f7da7a7b250e72e34cac781e
Laura Isabella <Laura Isabella@192.168.1.32>
2018-07-24 18:12:13 -0500

    prueba2

--- src/main/java/modelo/SolverProject.java
+++ src/main/java/modelo/SolverProject.java
@@ -9,7 +9,7 @@ import org.chocosolver.solver.variables.IntVar;
 
 public class SolverProject {
 	
-	//Constantes del Proyecto
+	//Constantes del Proyecto 1
 
 	public final static String TIPO_TICS="TICS";
 
