2be04caa05ce0c2cc464a8a69670e87b1db66634
davip <davip.95@hotmail.com>
2018-10-19 22:18:28 -0500

    commit viernes Sem 12

--- src/main/java/modelo/SolverProject.java
+++ src/main/java/modelo/SolverProject.java
@@ -18,21 +18,10 @@ public class SolverProject {
 
 	//Tipo de salas
 	public final static String TIPO_TICS="TIC";
-	//public final static String TIPO_LABORATORIO_REDES="Sala Redes";
 	public final static String TIPO_INDUSTRIAL="IND";
-	//public final static String TIPO_SALA_GENERAL="General";
-	//public final static String TIPO_SALA_GENERAL_DISE�O="General Dise�o";
-	public final static String TIPO_DISEÑO="DIS";
-	//public final static String TIPO_DISE�O_INDUSTRIAL="Dise�o Industrial";
+	public final static String TIPO_DISENO="DIS";
 	public final static String TIPO_IDIOMAS="IDI";
 	public final static String TIPO_FINANZAS="FIN";
-	//	public final static String TIPO_BIBLIO_SALA_PROFESORES="Biblioteca Sala Profesores";
-	//	public final static String TIPO_BIBLIO_BASES_DATOS="Biblioteca Bases de Datos";
-	//	public final static String TIPO_BIBLIO_CONSULTAS="Biblioteca Consultas";
-	//	public final static String TIPO_MAC="iMac";
-	//	public final static String TIPO_GRANJA_RENDER="Granja Render";
-	//public final static String TIPO_SALA_DOCTORADO="Sala de Doctorado";
-
 
 
 	//Arquitectura de los computadores
@@ -57,11 +46,11 @@ public class SolverProject {
 
 
 	//Procesadores
-	public final static String PROCESADOR_INTEL_CORE_I3="Intel� Core� i3";
-	public final static String PROCESADOR_INTEL_CORE_I5="Intel� Core� i5";
-	public final static String PROCESADOR_INTEL_CORE_I7="Intel� Core� i7";
-	public final static String PROCESADOR_INTEL_CORE_2DUO="Intel� Core� 2 DUO";
-	public final static String PROCESADOR_INTEL_XEON="Intel� XEON";
+	//	public final static String PROCESADOR_INTEL_CORE_I3="Intel� Core� i3";
+	//	public final static String PROCESADOR_INTEL_CORE_I5="Intel� Core� i5";
+	//	public final static String PROCESADOR_INTEL_CORE_I7="Intel� Core� i7";
+	//	public final static String PROCESADOR_INTEL_CORE_2DUO="Intel� Core� 2 DUO";
+	//	public final static String PROCESADOR_INTEL_XEON="Intel� XEON";
 
 	//Sistema Operativo
 	public final static String SIS_OP_WINDOWS10="Windows 10";
@@ -78,8 +67,8 @@ public class SolverProject {
 	public static void main(String[] args) {
 
 
-		//		leerCSVSalas();
-		//		leerCSVSoftware();
+		leerCSVSalas();
+		leerCSVSoftware();
 		modeloInicial();
 
 		//		for (int j = 0; j < toolSoftware.size(); j++) {
@@ -280,107 +269,29 @@ public class SolverProject {
 
 	}
 
-
-	public static void constructorModelo() {
-
-		//COMPUTADORES No utilizados
-		//COMPUTADORES DE TB
-		Computador computador1TBHD_2GBRAM_PROC_CORE2DUO= new Computador(PROCESADOR_INTEL_CORE_2DUO,ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_2GB, DISCO_DURO_1TB);
-
-
-		Computador computador1TBHD_4GBRAM_PROC_COREI3= new Computador(PROCESADOR_INTEL_CORE_I3,ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_4GB, DISCO_DURO_1TB);
-		Computador computador1TBHD_4GBRAM_PROC_COREI5= new Computador(PROCESADOR_INTEL_CORE_I5,ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_4GB, DISCO_DURO_1TB);
-		Computador computador1TBHD_4GBRAM_PROC_CORE2DUO= new Computador(PROCESADOR_INTEL_CORE_2DUO,ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_4GB, DISCO_DURO_1TB);
-
-
-		Computador computador1TBHD_8GBRAM_PROC_COREI5= new Computador(PROCESADOR_INTEL_CORE_I5, ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_8GB, DISCO_DURO_1TB);
-		Computador computador1TBHD_8GBRAM_PROC_COREI3= new Computador(PROCESADOR_INTEL_CORE_I3, ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_8GB, DISCO_DURO_1TB);
-		Computador computador1TBHD_8GBRAM_PROC_CORE2DUO= new Computador(PROCESADOR_INTEL_CORE_2DUO, ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_8GB, DISCO_DURO_1TB);
-
-
-		Computador computador1TBHD_10GBRAM= new Computador(PROCESADOR_INTEL_CORE_I5, ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_10GB, DISCO_DURO_1TB);
-
-		//COMPUTADORES DE GB
-		Computador computador150GBHD_3GBRAM= new Computador(PROCESADOR_INTEL_CORE_2DUO, ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_3GB, DISCO_DURO_150GB);
-		Computador computador150GBHD_4GBRAM= new Computador(PROCESADOR_INTEL_CORE_2DUO, ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_4GB,DISCO_DURO_150GB);
-
-		Computador computador250GBHD_2GBRAM= new Computador(PROCESADOR_INTEL_CORE_2DUO, ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_2GB, DISCO_DURO_250GB);		
-
-		Computador computador500GBHD_4GBRAM_PROC_COREI3= new Computador(PROCESADOR_INTEL_CORE_I3, ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_4GB, DISCO_DURO_500GB);
-		Computador computador500GBHD_4GBRAM_PROC_COREI5= new Computador(PROCESADOR_INTEL_CORE_I5, ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_4GB, DISCO_DURO_500GB);
-		Computador computador500GBHD_4GBRAM_PROC_CORE2DUO= new Computador(PROCESADOR_INTEL_CORE_2DUO, ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_4GB, DISCO_DURO_500GB);
-
-		Computador computador500GBHD_16GBRAM= new Computador(PROCESADOR_INTEL_XEON, ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_16GB, DISCO_DURO_500GB);
-		Computador computador750GBHD_8GBRAM= new Computador(PROCESADOR_INTEL_CORE_I7, ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_8GB, DISCO_DURO_750GB);
-
-
-		//COMPUTADORES Utilizados
-		//COMPUTADORES DE TB
-		Computador computador1TBHD_2GBRAM_PROC_COREI3= new Computador(PROCESADOR_INTEL_CORE_I3,ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_2GB, DISCO_DURO_1TB);
-
-		Computador computador1TBHD_16GBRAM_PROC_COREI5= new Computador(PROCESADOR_INTEL_CORE_I5,ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_16GB, DISCO_DURO_1TB);
-		Computador computador1TBHD_16GBRAM_PROC_CORE2DUO= new Computador(PROCESADOR_INTEL_CORE_2DUO,ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_16GB, DISCO_DURO_1TB);
-		Computador computador1TBHD_16GBRAM_PROC_XEON= new Computador(PROCESADOR_INTEL_XEON,ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_16GB, DISCO_DURO_1TB);
-
-		//COMPUTADORES DE GB
-		Computador computador250GBHD_4GBRAM= new Computador(PROCESADOR_INTEL_CORE_2DUO, ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_4GB, DISCO_DURO_250GB);
-
-		Computador computador500GBHD_2GBRAM_PROC_CORE2DUO= new Computador(PROCESADOR_INTEL_CORE_2DUO, ARQUITECTURA_32_BITS, SIS_OP_WINDOWS10, RAM_2GB, DISCO_DURO_500GB);
-
-		Computador computador500GBHD_8GBRAM_PROC_COREI3= new Computador(PROCESADOR_INTEL_CORE_I3, ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_8GB, DISCO_DURO_500GB);
-		Computador computador500GBHD_8GBRAM_PROC_COREI5= new Computador(PROCESADOR_INTEL_CORE_I5, ARQUITECTURA_64_BITS, SIS_OP_WINDOWS10, RAM_8GB, DISCO_DURO_500GB);
-
-
-		//Salas de Computo
-
-		//		salas.add(new Sala("Biblioteca", TIPO_BIBLIO_SALA_PROFESORES, 8 , computador250GBHD_4GBRAM));
-		//		salas.add(new Sala("Biblioteca", TIPO_BIBLIO_BASES_DATOS, 7 , computador250GBHD_2GBRAM));
-		//		salas.add(new Sala("Biblioteca", TIPO_BIBLIO_CONSULTAS, 8 , computador150GBHD_3GBRAM));
-		//		salas.add(new Sala("Biblioteca Equipos MAC", TIPO_MAC, 5 , computador500GBHD_4GBRAM_PROC_CORE2DUO));
-		//		salas.add(new Sala("Granja de Render", TIPO_GRANJA_RENDER, 23 , computador1TBHD_16GBRAM_PROC_XEON));
-		//salas.add(new Sala("Sala General Computo", TIPO_SALA_GENERAL, 61 , computador1TBHD_2GBRAM_PROC_COREI3));
-
-		salas= new ArrayList<Sala>();
-
-		salas.add(new Sala("201C", TIPO_IDIOMAS, 22 , computador500GBHD_2GBRAM_PROC_CORE2DUO));
-		salas.add(new Sala("202C", TIPO_TICS, 25 , computador1TBHD_8GBRAM_PROC_COREI5));
-		salas.add(new Sala("203C", TIPO_TICS, 30 , computador1TBHD_8GBRAM_PROC_COREI5));
-		salas.add(new Sala("205C", TIPO_INDUSTRIAL, 31 , computador1TBHD_8GBRAM_PROC_COREI5));
-		salas.add(new Sala("207C", TIPO_TICS, 30 , computador1TBHD_8GBRAM_PROC_COREI5));
-		salas.add(new Sala("208C", TIPO_TICS, 30 , computador500GBHD_8GBRAM_PROC_COREI5));
-		salas.add(new Sala("211C", TIPO_IDIOMAS, 22 , computador250GBHD_4GBRAM));
-
-		salas.add(new Sala("302C", TIPO_DISEÑO, 36 , computador1TBHD_16GBRAM_PROC_COREI5));
-		salas.add(new Sala("303C", TIPO_FINANZAS, 11 , computador1TBHD_16GBRAM_PROC_COREI5));
-		salas.add(new Sala("304C", TIPO_INDUSTRIAL, 36 , computador500GBHD_8GBRAM_PROC_COREI3));
-		salas.add(new Sala("305C", TIPO_INDUSTRIAL, 18 , computador1TBHD_8GBRAM_PROC_COREI5));
-		salas.add(new Sala("306C", TIPO_DISEÑO, 12 , computador1TBHD_16GBRAM_PROC_CORE2DUO));
-		//		salas.add(new Sala("307C", TIPO_TICS, 12 , computador750GBHD_8GBRAM));
-		//		salas.add(new Sala("308C", TIPO_TICS, 25 , computador1TBHD_16GBRAM_PROC_XEON));
-		salas.add(new Sala("309C", TIPO_INDUSTRIAL, 25 , computador1TBHD_16GBRAM_PROC_XEON));
-		salas.add(new Sala("310C", TIPO_DISEÑO, 25 , computador1TBHD_16GBRAM_PROC_XEON));
-		salas.add(new Sala("311C", TIPO_DISEÑO, 5 , computador1TBHD_16GBRAM_PROC_COREI5));
-
-		//		cargarInfoSoftware();
-
-	}
-
 	public static void cargarInfoSoftware() {
 
 		toolSoftware= new ArrayList<Software>();
 
 		toolSoftware.add(new Software("","",0,0,0,"Visual Paradigm", TIPO_TICS, "Intel Pentium 4", 1,ARQUITECTURA_32_BITS, "windows 10", 2, 4, "14.0",0,false,0));
-		toolSoftware.add(new Software("","",0,0,0,"3ds Max Studio", TIPO_INDUSTRIAL, "Intel o AMD multi-core",1, ARQUITECTURA_64_BITS, "windows 10", 4, 6, "2017",0,false,0));
-		toolSoftware.add(new Software("","",0,0,0,"Adobe Experience Design", TIPO_DISEÑO, "Intel o AMD multi-core",2, ARQUITECTURA_64_BITS, "windows 10", 4, 2, "0",0,false,0));
+		toolSoftware.add(new Software("","",0,0,0,"3ds Max Studio", TIPO_INDUSTRIAL, "Intel o AMD multi-core",1, ARQUITECTURA_64_BITS, "mac 10", 4, 6, "2017",0,false,0));
+		toolSoftware.add(new Software("","",0,0,0,"Adobe Experience Design", TIPO_DISENO, "Intel o AMD multi-core",2, ARQUITECTURA_64_BITS, "windows 10", 4, 2, "0",0,false,0));
 		toolSoftware.add(new Software("","",0,0,0,"Office", TIPO_TICS, "Intel",1, ARQUITECTURA_32_BITS, "windows 10", 4, 3, "2013",0,false,0));
 		toolSoftware.add(new Software("","",0,0,0,"Matlab", TIPO_TICS, " Intel o AMD x86-64", 1, ARQUITECTURA_64_BITS, "windows 10", 2, 2, "R2017a",0,false,0));
-		//toolSoftware.add(new Software("","",0,0,0,"Rosseta", TIPO_IDIOMAS, " Intel o AMD x86-64", 1, ARQUITECTURA_64_BITS, "windows 10", 2, 2, "R2017a",0,false,0));
+		toolSoftware.add(new Software("","",0,0,0,"Rosseta", TIPO_IDIOMAS, " Intel o AMD x86-64", 1, ARQUITECTURA_64_BITS, "windows 10", 2, 2, "R2017a",0,false,0));
+		toolSoftware.add(new Software("","",0,0,0,"Eclipse", TIPO_TICS, "Intel Pentium 4", 1,ARQUITECTURA_32_BITS, "windows 10", 2, 4, "14.0",0,false,0));
+		//		toolSoftware.add(new Software("","",0,0,0,"Age of Empires", TIPO_INDUSTRIAL, "Intel o AMD multi-core",1, ARQUITECTURA_64_BITS, "mac 10", 4, 6, "2017",0,false,0));
+		//		toolSoftware.add(new Software("","",0,0,0,"Creative Cloud", TIPO_DISENO, "Intel o AMD multi-core",2, ARQUITECTURA_64_BITS, "windows 10", 4, 2, "0",0,false,0));
+		//		toolSoftware.add(new Software("","",0,0,0,"SQL Developer", TIPO_TICS, "Intel",1, ARQUITECTURA_32_BITS, "windows 10", 4, 3, "2013",0,false,0));
+		//		toolSoftware.add(new Software("","",0,0,0,"Virtual Box", TIPO_TICS, " Intel o AMD x86-64", 1, ARQUITECTURA_64_BITS, "windows 10", 2, 2, "R2017a",0,false,0));
+		//		toolSoftware.add(new Software("","",0,0,0,"@Risk", TIPO_FINANZAS, " Intel o AMD x86-64", 1, ARQUITECTURA_64_BITS, "windows 10", 2, 2, "R2017a",0,false,0));
+
 	}
 
 	public static void modeloInicial() {
 
-		constructorModelo();
-		cargarInfoSoftware();
+		//constructorModelo();
+		//cargarInfoSoftware();
 
 		Model model= new Model();
 
@@ -392,19 +303,27 @@ public class SolverProject {
 
 			for (int j = 0; j < toolSoftware.size(); j++) {
 
-				if (toolSoftware.get(j).getNombre().equals("Office") && !salas.get(i).getNombre().equals("310C")) {
+				if (toolSoftware.get(j).getNombre().equals("Office") && !salas.get(i).getTipo().equals(TIPO_DISENO)) {
 
 					model.arithm(carrera[i][j], "=",1).post();
 				}
+
+				else if(toolSoftware.get(j).getSistemaOperativo().toUpperCase().contains("MAC") &&
+						salas.get(i).getComputadores().getSistemaOperativo().toUpperCase().contains("MAC")) {
+
+					model.arithm(carrera[i][j], "=",1).post();
+				}
+
 				else  if (!toolSoftware.get(j).getTipo().equals(salas.get(i).getTipo())) {
 
 					model.arithm(carrera[i][j], "=",0).post();
 				}
 
-				//				else if (toolSoftware.get(j).getTipo().equals(salas.get(i).getTipo()) && !toolSoftware.get(j).isEjecutable()) {
-				//
-				//					model.arithm(carrera[i][j], "=",1).post();
-				//				}
+				else  if (toolSoftware.get(j).getMemoriaRAM() > salas.get(i).getComputadores().getMemoriaRAM()) {
+
+					model.arithm(carrera[i][j], "=",0).post();
+				}
+
 			}
 		}
 
@@ -418,9 +337,9 @@ public class SolverProject {
 		//Mi PC 3000
 		//PC Liason 15000
 		solution.record();
-		Criterion solcpt = new SolutionCounter(model, 15000);
-		//List<Solution> list=model.getSolver().findAllSolutions(solcpt);
-		List<Solution> list=model.getSolver().findAllSolutions();
+		Criterion solcpt = new SolutionCounter(model, 100);
+		List<Solution> list=model.getSolver().findAllSolutions(solcpt);
+		//List<Solution> list=model.getSolver().findAllSolutions();
 
 		System.out.println("Primera parte");
 		System.out.println("Soluciones encontradas: "+list.size()+"\n");
@@ -428,7 +347,7 @@ public class SolverProject {
 		int nSol=1;
 		for(Solution s:list){
 
-			System.out.println("Soluci�n: " + nSol);
+			System.out.println("Soluci�n: " + nSol);
 			imprimirMatrizConSolucion(carrera, s);
 			solutionRecord=s;
 			nSol++;
@@ -436,7 +355,7 @@ public class SolverProject {
 
 		//}
 
-		System.out.println("0 = No se puede instalar"+"\n"+ "1 = Si instalar"+"\n"+"2 = Tiene ejecutable"+"\n");
+		System.out.println("0 = No se puede instalar"+"\n"+ "1 = Si instalar"+"\n");
 
 		restriccionesDeDisco(carrera,solutionRecord);
 
@@ -463,11 +382,6 @@ public class SolverProject {
 
 				}
 
-				else if (matrizCopia[i][j].getValue()==2) {
-
-					matrizPesos[i][j]=matrizCopia[i][j].mul(toolSoftware.get(j).getTamanoEjecutable()).div(2).intVar();
-				}
-
 			}
 		}
 
@@ -487,7 +401,7 @@ public class SolverProject {
 
 			for (int j = 0; j < toolSoftware.size(); j++) {
 
-				if (pesoPorSala>=8) {
+				if (pesoPorSala>=20) {
 
 					model.arithm(matrizResultado[i][j], "=", 0).post();
 					//System.out.println("Ojo, en la sala " +salas.get(i).getNombre()+" la cantidad de espacio de disco de software supera la capacidad de disco del computador ");
@@ -506,10 +420,84 @@ public class SolverProject {
 		model.getSolver().solve();
 		solution.record();
 
-		System.out.println("------------------------Matriz Resultado final------------------------");
-		imprimirMatrizConSolucion(matrizResultado, solution);
+		System.out.println("Resultado parcial");
+		//imprimirMatrizConSolucion(matrizResultado, solution);
+		imprimirMatriz(matrizResultado);
+
+		//restriccionesDeRAM(matrizResultado, solution);
 
 	}
+
+	//	public static void restriccionesDeRAM(IntVar[][] matrizCarreras, Solution solucionAnterior) {
+	//
+	//		System.out.println("Tercera parte"+"\n");
+	//
+	//		Model model= new Model();
+	//
+	//		IntVar[][] matrizPesosRam = model.intVarMatrix("pesosRam", salas.size(), toolSoftware.size(), 0, 1);
+	//		IntVar[][] matrizCopiaRam = model.intVarMatrix("pesosRam", salas.size(), toolSoftware.size(), 0, 1);
+	//		
+	//		matrizCopiaRam=copiarMatriz(solucionAnterior, matrizCarreras);
+	//
+	//		int count=0;
+	//
+	//		for (int i = 0; i < salas.size(); i++) {
+	//
+	//			for (int j = 0; j < toolSoftware.size(); j++) {
+	//
+	//					
+	//				if (matrizCopiaRam[i][j].getValue()==1) {
+	//
+	//					count++;
+	//					
+	//					matrizPesosRam[i][j]=matrizCopiaRam[i][j].mul(toolSoftware.get(j).getMemoriaRAM()).intVar();
+	//
+	//				}
+	//
+	//			}
+	//		}
+	//
+	//		System.out.println(count);
+	//		
+	//		System.out.println("Matriz de Pesos Memoria RAM:");
+	//		imprimirMatriz(matrizPesosRam);
+	//
+	////		IntVar[][] matrizResultado = model.intVarMatrix("pesos", salas.size(), toolSoftware.size(), 0, 1);
+	////		int pesoPorSala=0;
+	////
+	////		for (int i = 0; i < salas.size(); i++) {
+	////
+	////			for (int j = 0; j < toolSoftware.size(); j++) {
+	////
+	////				pesoPorSala+=matrizPesosRam[i][j].getValue();
+	////
+	////			}
+	////
+	////			for (int j = 0; j < toolSoftware.size(); j++) {
+	////
+	////				if (pesoPorSala>=20) {
+	////
+	////					model.arithm(matrizResultado[i][j], "=", 0).post();
+	////					//System.out.println("Ojo, en la sala " +salas.get(i).getNombre()+" la cantidad de espacio de disco de software supera la capacidad de disco del computador ");
+	////				} 
+	////
+	////				else if (matrizPesosRam[i][j].getValue() != 0) {
+	////
+	////					model.arithm(matrizResultado[i][j], "=", 1).post();
+	////
+	////				}
+	////			}
+	////			pesoPorSala=0;
+	////		}
+	////
+	////		Solution solution = new Solution(model);
+	////		model.getSolver().solve();
+	////		solution.record();
+	////
+	////		System.out.println("------------------------Matriz Resultado final------------------------");
+	////		imprimirMatrizConSolucion(matrizResultado, solution);
+	//
+	//	}
 
 
 	public static void imprimirMatriz(IntVar[][] matriz) {
@@ -520,7 +508,7 @@ public class SolverProject {
 
 				System.out.print(matriz[i][j].getValue());
 			}
-			
+
 			System.out.print(" "+salas.get(i).getNombre()+ " Tipo "+ salas.get(i).getTipo());
 			System.out.println("");
 
